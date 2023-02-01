e703b6a196858ce7a933b034b96e1945c93d6506
Harmcleff <33807817+Harmcleff@users.noreply.github.com>
2023-02-01 17:29:02 +0100

    Update 5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-""" a function that prints a text with 2 new lines after
-    each of these characters: ., ? and
-        Args:
-            .: first Args
-            ,: Second Args
-            ?: Third Args
-        Raise: TypeError text must be a string
-"""
+"""Defines a name-printing function."""
 
 
-def text_indentation(text):
-    if (".") in text:
-        print("\n")
-        print(end="")
-    elif ("?") in text:
-        print("\n")
-        print(text, end="")
-    elif (":") in text:
-        print("\n")
-        print(text, end="")
-    elif type(text) is not str:
-        raise TypeError("text must be a string")
+def say_my_name(first_name, last_name=""):
+    """Print a name.
+    Args:
+        first_name (str): The first name to print.
+        last_name (str): The last name to print.
+    Raises:
+        TypeError: If either of first_name or last_name are not strings.
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
