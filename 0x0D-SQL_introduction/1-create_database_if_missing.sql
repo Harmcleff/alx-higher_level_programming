06756209386b93edb1d4016c0390e240fd7bcab6
Harmcleff <33807817+Harmcleff@users.noreply.github.com>
2023-03-16 00:03:11 +0100

    Update 1-create_database_if_missing.sql

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,7 +1,6 @@
 -- a script that creates the database hbtn_0c_0 in your MySQL server
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 IF (SELECT COUNT(*) FROM information_schema.schemata WHERE schema_name = 'hbtn_0c_0') = 1 THEN
-    SELECT 'Database Already Exist';
+    SELECT 'Database already exists.';
 ELSE
-    SELECT 'Database Created Successfully';
+    SELECT 'Database created successfully.';
 END IF;
