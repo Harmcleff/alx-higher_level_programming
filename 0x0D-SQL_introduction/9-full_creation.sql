d88361649e2b7df9f23bc5f31a219dd6eef617f8
Harmcleff <33807817+Harmcleff@users.noreply.github.com>
2023-03-16 01:14:20 +0100

    Update 9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -17,5 +17,8 @@ CREATE TABLE IF NOT EXITS second_table(
      score INT
      );
      
-INSERT INTO second_table(id,name,score)
-VALUE(1,"John",10),(2,"Alex",3),(3,"Bob",14),(4,"George",8);
+INSERT INTO second_table(id,name,score)VALUES 
+(1,"John",10),
+(2,"Alex",3),
+(3,"Bob",14),
+(4,"George",8);
