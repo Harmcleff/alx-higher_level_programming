1a1b6fbcd1b4e9c3fc05fca94cb9e229777954a5
Sanyaolu Oladimeji H <33807817+Harmcleff@users.noreply.github.com>
2023-05-24 23:08:58 +0200

    Update 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,22 +1,18 @@
 #!/usr/bin/node
 
 const request = require('request');
+const fs = require('fs');
 const url = process.argv[2];
-const characterId = '18';
-let count = 0;
+const file = process.argv[3];
 
-request.get(url, (error, response, body) => {
+request(url, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
-    const data = JSON.parse(body);
-    data.results.forEach((film) => {
-      film.characters.forEach((character) => {
-        if (character.includes(characterId)) {
-          count += 1;
-        }
-      });
+    fs.writeFile(file, body, 'utf8', (error) => {
+      if (error) {
+        console.log(error);
+      }
     });
-    console.log(count);
   }
 });
